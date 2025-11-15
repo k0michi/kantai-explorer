66a9c18b2189fbca5a99a823ed3a8f44a6269479
k0michi <k0michi@koyomi.co>
2025-11-15 17:22:30 +0900

    Rewrite schema with zod

--- app/types/schema.ts
+++ app/types/schema.ts
@@ -1,40 +1,53 @@
-export type Coordinate = [number, number];
+import { z } from "zod";
 
-export type Place = {
-  name: string;
-  coordinate: Coordinate;
-  references?: string[];
-};
+export const CoordinateSchema = z.tuple([z.number(), z.number()]);
+export type Coordinate = z.infer<typeof CoordinateSchema>;
 
-export type PlaceRef = string | { coordinate: Coordinate; };
+export const PlaceSchema = z.object({
+  name: z.string(),
+  coordinate: CoordinateSchema,
+  references: z.string().array().optional(),
+});
+export type Place = z.infer<typeof PlaceSchema>;
 
-export type VesselType = 'battleship';
+export const PlaceRefSchema = z.union([
+  z.string(),
+  z.object({ coordinate: CoordinateSchema })
+]);
+export type PlaceRef = z.infer<typeof PlaceRefSchema>;
 
-export type VesselEvent = {
-  name?: string;
-  date: string;
-  place: PlaceRef;
-  references?: string[];
-};
+export const VesselTypeSchema = z.enum(["battleship"]);
+export type VesselType = z.infer<typeof VesselTypeSchema>;
 
-export type Vessel = {
-  name: string;
-  affiliation: string;
-  type: VesselType;
-  events: VesselEvent[];
-};
+export const VesselEventSchema = z.object({
+  name: z.string().optional(),
+  date: z.string(),
+  place: PlaceRefSchema,
+  references: z.string().array().optional(),
+});
+export type VesselEvent = z.infer<typeof VesselEventSchema>;
 
-export type Event = {
-  name: string;
-  date?: string;
-  begin_date?: string;
-  end_date?: string;
-  place: PlaceRef;
-  references?: string[];
-};
+export const VesselSchema = z.object({
+  name: z.string(),
+  affiliation: z.string(),
+  type: VesselTypeSchema,
+  events: VesselEventSchema.array(),
+});
+export type Vessel = z.infer<typeof VesselSchema>;
 
-export type Data = {
-  places: Record<string, Place>;
-  vessels: Record<string, Vessel>;
-  events: Event[];
-};
+export const EventSchema = z.object({
+  name: z.string(),
+  date: z.string().optional(),
+  begin_date: z.string().optional(),
+  end_date: z.string().optional(),
+  place: PlaceRefSchema,
+  references: z.string().array().optional(),
+});
+export type Event = z.infer<typeof EventSchema>;
+
+export const DataSchema = z.object({
+  places: z.record(z.string(), PlaceSchema),
+  vessels: z.record(z.string(), VesselSchema),
+  events: EventSchema.array(),
+});
+export type Data = z.infer<typeof DataSchema>;
